cbbef8cd8bb16c58acb23a23acbf8883101f1f68
Lukas Meyer <lukas.meyer@fau.de>
2022-11-22 14:03:37 +0100

    changed githuc action and added test file

---
+++ pc_skeletor/test.py
@@ -0,0 +1,16 @@
+# Own modules
+from pc_skeletor.skeletor import *
+from pc_skeletor.download import *
+
+if __name__ == "__main__":
+    downloader = Dataset()
+    downloader.download_tree_dataset()
+
+    # Init tree skeletonizer
+    skeletor = Skeletonizer(point_cloud=downloader.file_path,
+                            down_sample=0.01,
+                            debug=False)
+    laplacian_config = {"MAX_LAPLACE_CONTRACTION_WEIGHT": 1024,
+                        "MAX_POSITIONAL_WEIGHT": 1024,
+                        "INIT_LAPLACIAN_SCALE": 100}
+    sceleton = skeletor.extract(method='Laplacian', config=laplacian_config)
