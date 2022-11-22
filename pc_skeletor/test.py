a6f44b586bef798ae8461987958fc5638b7a40d2
Lukas Meyer <lukas.meyer@fau.de>
2022-11-22 14:13:45 +0100

    changed path for testing

--- pc_skeletor/test.py
+++ pc_skeletor/test.py
@@ -1,6 +1,6 @@
 # Own modules
-from .skeletor import *
-from .download import *
+from skeletor import *
+from download import *
 
 if __name__ == "__main__":
     downloader = Dataset()
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
     # Init tree skeletonizer
     skeletor = Skeletonizer(point_cloud=downloader.file_path,
-                            down_sample=0.01,
+                            down_sample=0.05,
                             debug=False)
     laplacian_config = {"MAX_LAPLACE_CONTRACTION_WEIGHT": 1024,
                         "MAX_POSITIONAL_WEIGHT": 1024,
