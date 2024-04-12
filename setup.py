35860ad6339e1ce0c46e906a02c77129854b7eeb
Lukas Meyer <lukas.meyer@fau.de>
2024-04-12 10:23:36 +0200

    requirements

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
     url="https://github.com/meyerls/PC-Skeletor",
     packages=find_packages(),
     install_requires=install_requires,  # external packages as dependencies
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     classifiers=[
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,5 @@
 conda create --name s-lbc python=3.8 -y
 conda activate s-lbc
+conda install conda-forge::gfortran
 pip install --upgrade pip setuptools
 pip install -r requirements.txt
